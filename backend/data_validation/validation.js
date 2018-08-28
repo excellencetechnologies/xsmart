34c03d854e53d512fba4f21744d2ae110086b014
Arun Kumar <exceltes@gmail.com>
2018-08-28 16:19:28 +0530

    fix error msg

--- backend/data_validation/validation.js
+++ backend/data_validation/validation.js
@@ -1,5 +1,5 @@
 async function validateDeviceData(checkBody, validationErrors, body) {
-    checkBody('chip_id').notEmpty().withMessage("required city");
+    checkBody('chip_id').notEmpty().withMessage("required chip id");
     let errors = validationErrors();
     if (errors) {
         return new Error("error : please provide chip id of the device");
