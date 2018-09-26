3f8cf1fcdcaba840657094b2b05242b257d576ae
excellencetechnologies <excellencetechnologies08@gmail.com>
2018-09-26 14:54:18 +0530

    update

--- ionic/src/app/set-wifi-password/set-wifi-password.component.ts
+++ ionic/src/app/set-wifi-password/set-wifi-password.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from '@angular/router';
+import { ApiService } from '../api/api.service';
+import { Ping, Wifi, Device, Switch } from "../api/api"
+import { NotifyService } from '../api/notify.service';
+import { NavParams, ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-set-wifi-password',
   templateUrl: './set-wifi-password.component.html',
   styleUrls: ['./set-wifi-password.component.scss']
 })
 export class SetWifiPasswordComponent implements OnInit {
+  wifinetworks: Wifi[] = [];
   passwordForm: FormGroup;
   loading:boolean;
   errorMessage:string;
-  constructor( private router: Router) { }
+  loader:boolean;
+  isScanningDevice: boolean = false;
+  ssid:any;
+  constructor( 
+    private router: Router,
+    private api:ApiService,
+    private notifyService: NotifyService,
+    private navParams: NavParams,
+    public modalController: ModalController
+  ) { }
 
   ngOnInit() {
     this.createLoginForm();
+   this.getSsid();
+  }
+  getSsid(){
+   this.ssid= this.navParams.get('ssid')
   }
   createLoginForm() {
     this.passwordForm = new FormGroup({
@@ -27,10 +45,14 @@ export class SetWifiPasswordComponent implements OnInit {
     try {
       this.loading = false;
       this.passwordForm.reset();
-      this.router.navigate([""]);
+      await this.api.setWifiPassword(this.ssid.wifi,formData.password);
+      await this.modalController.dismiss();
+      this.router.navigate(["/tabs"]);
     } catch (err) {
       this.loading = false;
       this.errorMessage = err['error'];
     }
   }
+
+
 }
