47cd642369978a2ace0d046b952451d4e07c4469
manish <manish>
2018-10-13 16:43:12 +0530

    fixing

--- backend/websocket-v2.js
+++ backend/websocket-v2.js
@@ -269,13 +269,15 @@ ws.on('connection', function (w) {
                 let chip = obj['chip'];
                 let offset = new Date().getTimezoneOffset();
                 let time = new Date().getTime() + offset * 60 * 1000;
-                if (!devices[chip]){
+                if (!devices[chip]) {
                     devices[chip] = {};
-                    let device = new Device;
-                    device.chip = obj['chip'];
-                    device.user_id = -1;
-                    device.meta = {};
-                    await device.save();
+                    if (!await Device.findOne({ chip: obj['chip'] }).lean().exec()) {
+                        let device = new Device;
+                        device.chip = obj['chip'];
+                        device.user_id = -1;
+                        device.meta = {};
+                        await device.save();
+                    }
                 }
 
                 let deviceDB = false;
