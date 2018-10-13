11a88f15bc2670695e3dedb85132744b5c2dc80d
manish <manish>
2018-10-13 17:40:35 +0530

    fixing

--- backend/websocket-v2.js
+++ backend/websocket-v2.js
@@ -364,7 +364,7 @@ ws.on('connection', function (w) {
 
                 Object.keys(devices).forEach((c) => {
                     if (c === chip) {
-                        console.log(c, "XXXXXXXXXXX");
+                        console.log(c, "XXXXXXXXXXX", chip);
                         console.log(devices[c]);
                         w.send(JSON.stringify({
                             type: "device_online_check_reply",
