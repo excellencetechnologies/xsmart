c956070e441ee145e69138d6593e69f3a967bc07
manish <manish>
2018-10-13 13:57:09 +0530

    fixing

--- backend/websocket-v2.js
+++ backend/websocket-v2.js
@@ -204,11 +204,11 @@ handleProtocol = async (obj, ws, w) => {
             let date = obj['time'].split('-')[1].split("/");
             console.log(time);
             console.log(date);
-            console.log(new Date(2000 + date[2]*1, date[1], date[0], time[0], time[1], time[2]));
+            console.log(new Date(2000 + date[2]*1, date[0]*1 - 1, date[1], time[0], time[1], time[2]));
             let attendance = new Attendance;
             attendance.chip = obj['chip'];
             attendance.emp_id = obj['emp_id'];
-            attendance.time = new Date(2000 + date[2]*1, date[1], date[0], time[0], time[1], time[2]);
+            attendance.time = new Date(2000 + date[2]*1, date[0]*1 - 1, date[1], time[0], time[1], time[2]);
             attendance.save();
 
         } else {
