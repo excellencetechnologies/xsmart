1d59764b2b9d8e7675a733bf63afbbb37ed76de7
manish <manish>
2018-10-17 16:16:08 +0530

    fixes

--- backend/routes/card.js
+++ backend/routes/card.js
@@ -19,6 +19,18 @@ router.post("/addTime", async (req, res) => {
             console.log(data);
             if (data.length == 4) {
 
+                console.log({
+                    url: "http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php",
+                    method: "POST",
+                    json: {
+                        "action": "employee_punch_time",
+                        "secret_key": "a5e03eaf60684b64793f4e38f958b3e1",
+                        // "user_id": req.params.data[2],
+                        "user_id": 299,
+                        "punch_time": data[3]
+                    }
+                })
+
                 await request({
                     url: "http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php",
                     method: "POST",
